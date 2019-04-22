9b7bb77a82b26c6954253f7a97720adb9d69e447
Jackson Ryan <jackson.baker.ryan@gmail.com>
2019-04-22 10:57:56 -0700

    Init map component with mapbox-gl

---
+++ src/components/TheMap.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div id="the-map"></div>
+  </div>
+</template>
+
+<script>
+import mapboxgl from "mapbox-gl";
+
+export default {
+  name: "TheMap",
+  props: {},
+  data: function() {
+    return {
+      map: new mapboxgl.Map({
+        container: "the-map",
+        style: "mapbox://styles/mapbox/streets-v9"
+      })
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/TheMapContainer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <the-map></the-map>
+  </div>
+</template>
+
+<script>
+import TheMap from "./components/TheMap";
+
+export default {
+  name: "TheMapContainer",
+  props: {},
+  data: function() {
+    return {};
+  },
+  components: {
+    TheMap
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
