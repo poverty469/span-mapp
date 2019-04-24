8ccbebebb1c277ebf35290715d01edab072bfb60
Jackson Ryan <jackson.baker.ryan@gmail.com>
2019-04-23 20:42:03 -0700

    Add test geojson data and style

--- src/components/TheMap.vue
+++ src/components/TheMap.vue
@@ -7,6 +7,7 @@
 <script>
 import mb from "mapbox-gl";
 import boundsEnum from "../../mock-data/bounds";
+import legislativeLayer from "../../mock-data/layer";
 
 export default {
   name: "TheMap",
@@ -20,7 +21,35 @@ export default {
     mb.accessToken = process.env.VUE_APP_MAPBOX_API_ACCESS_TOKEN;
     this.map = new mb.Map({
       container: "the-map",
-      style: "mapbox://styles/jacksonrya/cjutds7pm2jq71fom14q9x1ws",
+      style:
+        process.env.NODE_ENV == "development"
+          ? {
+              version: 8,
+              sources: {
+                "simple-tiles": {
+                  type: "raster",
+                  // point to our third-party tiles. Note that some examples
+                  // show a "url" property. This only applies to tilesets with
+                  // corresponding TileJSON (such as mapbox tiles).
+                  tiles: [
+                    // "http://a.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                    // "http://b.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    "http://tile.stamen.com/toner-lite/{z}/{x}/{y}.png"
+                  ],
+                  tileSize: 256
+                }
+              },
+              layers: [
+                {
+                  id: "simple-tiles",
+                  type: "raster",
+                  source: "simple-tiles",
+                  minzoom: 0,
+                  maxzoom: 22
+                }
+              ]
+            }
+          : "mapbox://styles/jacksonrya/cjuunpxmpbetg1fo7bniepqq4",
       bounds: new mb.LngLatBounds(
         boundsEnum.washington.sw,
         boundsEnum.washington.ne
@@ -28,6 +57,10 @@ export default {
       fitBoundsOptions: {
         padding: this.$store.getters.mapFocusPadding
       }
+    });
+
+    this.map.on("load", () => {
+      this.addGeoJsonLayer("districts", legislativeLayer.geometry);
     });
   },
   methods: {
@@ -42,6 +75,39 @@ export default {
       this.map.fitBounds(bounds, {
         padding: this.$store.getters.mapFocusPadding
       });
+    },
+    /**
+     * @param {String} id The name id to associate with the layer.
+     * @param {Geojson} data The GeoJson source for the layer.
+     */
+    addGeoJsonLayer(id, data) {
+      // Add geojson source
+      this.map.addSource(id, {
+        type: "geojson",
+        data: data
+      });
+
+      // Add polygon layer
+      this.map.addLayer({
+        id: id + "-polygons",
+        type: "fill",
+        source: id,
+        paint: {
+          "fill-color": "rgb(220, 174, 96)",
+          "fill-opacity": 0.6
+        }
+      });
+
+      // Add outline layer
+      this.map.addLayer({
+        id: id + "-lines",
+        type: "line",
+        source: id,
+        paint: {
+          "line-color": "rgb(139, 103, 41)",
+          "line-width": 2
+        }
+      });
     }
   }
 };
@@ -55,5 +121,6 @@ export default {
   left: 0;
   width: 100%;
   height: 100%;
+  color: rgb(220, 174, 96);
 }
 </style>
