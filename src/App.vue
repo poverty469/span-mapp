c6054734e6a88c372aaf588de6868e8fdd4e70a3
Jackson Ryan <jackson.baker.ryan@gmail.com>
2019-04-24 16:11:28 -0700

    Add sample topic data

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <the-map-dashboard class="map-container"></the-map-dashboard>
+    <the-story-dashboard></the-story-dashboard>
   </div>
 </template>
 
 <script>
 import TheMapDashboard from "./components/TheMapDashboard.vue";
+import TheStoryDashboard from "./components/TheStoryDashboard.vue";
 
 export default {
   name: "app",
   components: {
-    TheMapDashboard
+    TheMapDashboard,
+    TheStoryDashboard
   }
 };
 </script>
---
+++ src/components/TheStoryDashboard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "TheStoryDashboard",
+  props: {},
+  data: function() {
+    return {};
+  },
+  components: {}
+};
+</script>
+
+<style lang="scss"></style>
