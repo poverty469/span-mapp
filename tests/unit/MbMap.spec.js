fc4d0e68dbb60a9399283b782b975d7458c4c40d
Jackson Baker Ryan <jackryan@uw.edu>
2019-05-09 09:47:49 -0700

    Add more mapbox testing support
    - Install jest-canvas-mock to mock the DOM canvas used in mapbox
    - Add mapbox mock to MbMap unit test
    - Mocking is still broken

--- tests/unit/MbMap.spec.js
+++ tests/unit/MbMap.spec.js
@@ -1,7 +1,52 @@
-import { shallowMount } from "@vue/test-utils";
+import Vuex from "vuex";
+import { shallowMount, createLocalVue } from "@vue/test-utils";
 import MbMap from "@/components/MbMap.vue";
 
+// mapbox-gl/dist/mapbox-gl
+jest.mock("mapbox-gl", () => ({
+  GeolocateControl: jest.fn(),
+  Map: jest.fn(() => ({
+    addControl: jest.fn(),
+    on: jest.fn(),
+    remove: jest.fn(),
+    addLayer: jest.fn(),
+    removeLayer: jest.fn(),
+    addSource: jest.fn(),
+    fitBounds: jest.fn(),
+    getCanvas: jest.fn(),
+    setFeatureState: jest.fn()
+  })),
+  NavigationControl: jest.fn(),
+  AttributionControl: jest.fn(),
+  ScaleControl: jest.fn(),
+  Popup: jest.fn()
+}));
+
+const localVue = createLocalVue();
+
+localVue.use(Vuex);
+
 describe("MbMap.vue", () => {
+  let getters;
+  let store;
+
+  beforeEach(() => {
+    getters = {
+      mapFocusPadding: () => {
+        return {
+          top: 100,
+          right: 100,
+          bottom: 100,
+          left: 100
+        };
+      }
+    };
+
+    store = new Vuex.Store({
+      getters
+    });
+  });
+
   it("does not allow mixed geographies", () => {
     const wrapper = shallowMount(MbMap, {
       propsData: {
@@ -10,15 +55,17 @@ describe("MbMap.vue", () => {
           {
             dataset: {},
             geography: "districts"
-          },
-          {
-            dataset: {},
-            geography: "counties"
           }
+          // {
+          //   dataset: {},
+          //   geography: "counties"
+          // }
         ]
-      }
+      },
+      store,
+      localVue
     });
-    console.log(wrapper.options);
-    // expect(wrapper.text()).toMatch(msg);
+    console.log("wrapper text...", wrapper.text());
+    // expect(wrapper.text()).toMatch(msg);)
   });
 });
