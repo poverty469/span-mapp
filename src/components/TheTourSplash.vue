098d8a3603f442c9cf160670c9fda3203389718f
Jackson Baker Ryan <jackryan@uw.edu>
2019-05-16 21:35:04 -0700

    Style tour splash buttons

--- src/components/TheTourSplash.vue
+++ src/components/TheTourSplash.vue
@@ -4,47 +4,52 @@
     <transition name="fade-long">
       <div v-show="appLoading" class="the-splash-page__background"></div>
     </transition>
-    <div class="splash-page-container--loaded">
-      <div
-        class="splash-page__text-container"
-        :class="{ 'splash-page__text-container--not-loading': !appLoading }"
-      >
-        <p class="splash-page__text">
-          <b>
-            Poverty is complex, and the reasons surrounding its existence are
-            deeply historical and multifaceted. At Poverty Action, we believe
-            that poverty can be solved through progressive and inclusive policy
-            legislation. However, in order to pass anti-poverty reforms that are
-            both equitable and sustainable, we must first understand the scope
-            of poverty in our state.
-          </b>
-          <br />
-          <br />This interactive map allows users to explore the social,
-          economic, and historical elements behind poverty in Washington state.
-          <br />
-          <br />Our map shows the spread of poverty through a variety of
-          factors, and includes stories of people affected by poverty in our
-          state. In order to make the most of your time, we invite you to
-          <b>take the tour of the state of poverty in Washington.</b>
-        </p>
-      </div>
-      <transition name="fade-long">
-        <button
-          v-show="!appLoaded"
-          class="splash-page__start-tour-button splash-page__button"
-          @click="handleStartTourButton"
+    <div class="splash-page">
+      <div class="splash-page__container">
+        <div
+          class="splash-page__card"
+          :class="{ 'splash-page__card--loading': appLoading }"
         >
-          Start Tour
-        </button>
-      </transition>
+          <p class="splash-page__text">
+            <b>
+              Poverty is complex, and the reasons surrounding its existence are
+              deeply historical and multifaceted. At Poverty Action, we believe
+              that poverty can be solved through progressive and inclusive
+              policy legislation. However, in order to pass anti-poverty reforms
+              that are both equitable and sustainable, we must first understand
+              the scope of poverty in our state.
+            </b>
+            <br />
+            <br />This interactive map allows users to explore the social,
+            economic, and historical elements behind poverty in Washington
+            state.
+            <br />
+            <br />Our map shows the spread of poverty through a variety of
+            factors, and includes stories of people affected by poverty in our
+            state. In order to make the most of your time, we invite you to
+            <b>take the tour of the state of poverty in Washington.</b>
+          </p>
+        </div>
+        <transition name="fade-long">
+          <a
+            v-show="!appLoading"
+            class="splash-page__start-tour-button splash-page__button splash-page__card"
+            @click="handleStartTourButton"
+            >Start the Tour</a
+          >
+        </transition>
+      </div>
+
       <transition name="fade-long">
         <router-link
           v-show="!appLoading"
           to="map"
-          class="splash-page__start-tour-button splash-page__button"
+          class="splash-page__skip-tour-button splash-page__button splash-page__card"
           @click.native="handleSkipTourButton"
-          >Skip Tour</router-link
         >
+          Not your first visit?
+          <b>Skip the tour</b>
+        </router-link>
       </transition>
     </div>
   </div>
@@ -74,12 +79,8 @@ export default {
 
 <style lang="scss" scoped>
 /* Reference index.html for css */
-.splash-page-container--loaded {
-  position: fixed;
-  width: $app-width;
-  height: $app-height;
-  top: 0;
-  left: 0;
+.splash-page {
+  pointer-events: none;
 }
 
 .the-splash-page__background {
@@ -91,15 +92,52 @@ export default {
   background: $light-orange;
 }
 
-.splash-page__text-container {
-  border: 3px solid white;
+.splash-page__card {
+  border: 3px solid $dark-grey;
+  box-shadow: $standard-box-shadow;
 }
 
-.splash-page__text-container--not-loading {
-  border: 3px solid $dark-grey;
+.splash-page__card--loading {
+  box-shadow: none;
 }
 
 .splash-page__button {
+  text-align: center;
+  text-transform: uppercase;
   pointer-events: auto;
+  cursor: pointer;
+  user-select: none;
+}
+
+.splash-page__start-tour-button {
+  display: block;
+  margin-top: 2rem;
+  padding: 15px 40px;
+  background: $dark-orange;
+  font-size: 3rem;
+}
+
+.splash-page__start-tour-button:hover {
+  background: $light-orange;
+}
+
+.splash-page__start-tour-button:active {
+  background: $dark-orange;
+}
+
+.splash-page__skip-tour-button {
+  position: absolute;
+  right: 0;
+  bottom: $footer-height;
+  margin: 15px 25px;
+  padding: 7px 15px;
+}
+
+.splash-page__skip-tour-button:hover {
+  background: $dark-orange;
+}
+
+.splash-page__skip-tour-button:active {
+  background: $dark-orange--shadow;
 }
 </style>
