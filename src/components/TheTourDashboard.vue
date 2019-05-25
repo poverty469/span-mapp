dd45b8e992803dc1d92bf44710a5338e9b24db6e
jinc132 <jinc132@u.washington.edu>
2019-05-25 15:17:02 -0700

    Add info progress

--- src/components/TheTourDashboard.vue
+++ src/components/TheTourDashboard.vue
@@ -74,7 +74,8 @@ export default {
       activeTourSlideIndex: 0,
       mapLoaded: false,
       mapVisible: false,
-      arr: [{Obj: 10, tut: 1}, {Obj: 12, tut: 2}]
+      arr: [{Obj: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", tut: 10},
+      {Obj: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", tut: "Loream sunt in culpa qui officia deserunt mollit anim id est laborum."}]
     };
   },
   methods: {
