796230ecb2dd7ef545153255a75a27409be5fac8
Jackson Baker Ryan <jackryan@uw.edu>
2019-05-24 18:28:27 -0700

    Remove empty layer data objects in tour slide data

--- src/assets/data/TourSlides.js
+++ src/assets/data/TourSlides.js
@@ -1,3 +1,4 @@
+import { MapTypes } from "@/util/enums";
 import geographies from "@/assets/geographies";
 import povertyData from "@/assets/data/dataLayer";
 
@@ -5,9 +6,9 @@ const tourSlideData = [
   {
     title: "Test Slide Title",
     subtitle: "Test subtitle",
-    narrativeHtml:  () => {
-      return 
-        ```
+    narrativeHtml: () => {
+      return;
+      ```
         <p>
             <a href="https://povertyaction.org/listening-sessions/*" target="_blank"
                 In our conversations with people across the stat
@@ -33,10 +34,10 @@ const tourSlideData = [
         <p>
             Poverty Action works to advocate for and advance progressive policy solutions that fix our state tax code, strengthen state basic needs programs, create robust consumer protections, and offer people with criminal convictions a second chance.
         </p>
-        ```
-      },
+        ```;
+    },
     tutorialHtml: () => {
-      return 
+      return;
       ```
         <p>
           Poverty Action works to advocate for and advance progressive policy solutions that fix our state tax code.
@@ -45,54 +46,56 @@ const tourSlideData = [
         <p>
           Try this thing over there.
         </p>
-      ```
+      ```;
     },
-    dataLayers: [{
-      dataset: povertyData,
-      geographyId: geographies.counties.id,
-      attributeId: "HC03_VC161",
-      style: "choropleth"
-    }]
+    dataLayers: [
+      {
+        dataset: povertyData,
+        geographyId: geographies.counties.id,
+        attributeId: "HC03_VC161",
+        type: MapTypes.CHOROPLETH
+      }
+    ]
   },
   {
     title: "Living in Poverty",
     subtitle: "",
-    narrativeHtml:  () => {
-      return "Although poverty rates have been falling both in Washington state and nationwide, "
+    narrativeHtml: () => {
+      return "Although poverty rates have been falling both in Washington state and nationwide, ";
     },
     tutorialHtml: "Try out this thing",
-    dataLayers: [{}]
+    dataLayers: []
   },
   {
     title: "Median Incomes",
     subtitle: "by County",
     narrativeHtml: () => {
-      return "<p>Although certain sectors of our state’s economy are booming, wages have not risen for everyone.</p>"
+      return "<p>Although certain sectors of our state’s economy are booming, wages have not risen for everyone.</p>";
     },
     tutorialHtml: "<p>Try out that thing</p><p>Then try out that thing</p>",
-    dataLayers: [{}]
+    dataLayers: []
   },
   {
     title: "Median Cost of Monthly Rent",
     subtitle: "by County",
     narrativeHtml: () => {
-        return ```
+      return ```
         <p>
             <a href="https://seattle.curbed.com/2018/6/14/17464900/seattle-washington-minimum-wage-housing-costs" target="_blank">
                 Wages have not kept pace with rapidly rising costs of living throughout the state
             </a>
             . For many Washingtonians living on low or moderate wages, monthly rent costs take an unsustainable portion of their take-home pay.
         </p>
-        ```
+        ```;
     },
     tutorialHtml: "<p>Try out that thing</p><p>Then try out that thing</p>",
-    dataLayers: [{}]
+    dataLayers: []
   },
   {
     title: "Listening Sessions",
     narrativeHtml: () => {
-      return 
-        ```
+      return;
+      ```
         <p>
             <a href="https://povertyaction.org/listening-sessions/*" target="_blank"
                 In our conversations with people across the stat
@@ -118,9 +121,9 @@ const tourSlideData = [
         <p>
             Poverty Action works to advocate for and advance progressive policy solutions that fix our state tax code, strengthen state basic needs programs, create robust consumer protections, and offer people with criminal convictions a second chance.
         </p>
-        ```
-      }
-}
+        ```;
+    }
+  }
 ];
 
 export default tourSlideData;
