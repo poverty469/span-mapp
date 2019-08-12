1970baea2394b6cda0e6966a305a2138701d7fd3
Adriana Lasso-Harrier <adrianalasso-harrier@Adrianas-MacBook-Air.local>
2019-08-11 21:12:53 -0700

    Changes to file

--- src/assets/data/faq.js
+++ src/assets/data/faq.js
@@ -3,12 +3,12 @@ const Faq = [
         title: 'General',
         data: [
             {
-                question: 'Who can I contact to share my story?',
-                answer: 'You can email your story to Statewide Poverty Action Network through this email: <a href="adriana@povertyaction.org">adriana@povertyaction.org</a>'
+                question: 'Why did you choose to present these particular facets of poverty?',
+                answer: 'There are many ways to visualize (or map) poverty in a particular region. For this specific map, we chose to highlight data sets that corresponded with the four policy campaigns that our organization has historically worked on.'
             },
             {
-                question: 'Question',
-                answer: 'Answer'
+                question: 'I have a story I want to share! How can I do so?',
+                answer: 'You can email your story to our Communications Manager, Adriana: <a href="adriana@povertyaction.org">adriana@povertyaction.org</a>'
             }
         ]
     },
@@ -16,28 +16,21 @@ const Faq = [
         title: 'Data',
         data: [
             {
-                question: 'What are the Data Sources?',
-                answer: 'The data was obtained from the U.S. Census Bureau'
+                question: 'What are the data sources?',
+                answer: 'The data was obtained primarily from the U.S. Census Bureau. We also obtained data from the Department of Health and Human Services (DSHS) and from research done by the University of Washington on the Self Sufficiency Standard.'
             },
             {
-                question: 'Question',
-                answer: 'Answer'
+                question: 'Why do you use the Self Sufficiency Standard as well as the Federal Poverty Level Don`t they mean the same thing?',
+                answer: 'Great question! The Federal Povery Level (FPL) and the Self Sufficiency Standard (SSS) are two separate measurements that tell two very different stories. The FPL is a measurement that the federal government uses to determine who is eligible for assistance programs. The FPL is updated yearly based on information gathered by the US Census Bureau. However, the federal government is using the same standards and guidelines to determine the FPL that it did when it was created in the 1960s, that do not account for modern economic realities. The SSS was created as answer to the shortcomings of the FPL, and takes a variety of variables, such as geographic differences and ages of children, into account when calculating the income level that households much reach to meet their basic needs. We included the FPL in this map because it is a very real measure that determines who is eligible for what basic needs assistance programs, and we also included the SSS as a measure that illuminates lived economic experience.'
             }
         ]
     },
     {
-        title: 'Privacy',
-        data: [
-            {
-                question: 'Question',
-                answer: 'Answer'
-            },
-            {
-                question: 'Question',
-                answer: 'Answer'
+    
+        
             }
         ]
-    }
-];
+    
+;
 
 export default Faq;
--- src/components/TheAbout.vue
+++ src/components/TheAbout.vue
@@ -1,21 +1,17 @@
 <template>
   <div class="about-container">
     <p class="about-paragraph">
-        Solid Ground (founded in 1974 as the Fremont Public Association) is an anti-poverty and social service
-        organization in Seattle, Washington. Originating in Seattle's Fremont neighborhood and now based in the 
-        adjacent Wallingford neighborhood, the organization now serves clients throughout Seattle, King County 
-        and (for some programs) statewide. One of Solid Ground's advocacy programs is the Statewide Poverty Action
-        Network. They work to create systemic change to address root causes of poverty. <br /><br /> This program was
-        established in 1996, in response to the federal government’s passage of the Personal Responsibility and
-        Work Opportunity Reconciliation Act (PRWORA), or “welfare reform.” Ten leaders from communities of color
-        came together to form the Washington Welfare Reform Coalition, which in 2001 changed its name to the Statewide
-        Poverty Action Network. The change was made to better to represent an expanded focus on poverty issues of statewide
-        significance.<br /><br />Poverty Action is a leader in statewide issues and maintains a full-time lobby presence in
-        Olympia. With over 7,000 individual members of their network, Poverty Action is among the largest non-profit
-        advocacy groups in Washington state. In addition to political advocacy, Poverty Action also coordinates the "Vote 
-        for A Change Campaign"- An award winning program focused on engaging people living with low-incomes, the previously 
-        incarcerated and communities of color in electoral politics. Over 100,000 Washingtonians have been reached through 
-        "Vote for A Change" Campaigns from 2004-2011.
+        
+The Statewide Poverty Action Network is an anti-poverty advocacy organization located in Seattle, Washington.
+<br/><br/>
+Through in-person community organizing and online advocacy, we advance equitable policy solutions for low-income people in Washington state.
+<br/><br/>
+Poverty is a vast issue that is often difficult to define. With this in mind, we chose to create an interactive map that illustrates poverty in Washington state through the lens of the four policy campaigns that our organization works on. Visitors to this map will be guided through a picture of the economic realities for Washington families through depictions of the variable costs of living throughout the state, median income levels, median housing costs, rates of homelessness, and more. Personal story is central to our organization's mission, and we have included the stories of Washingtonians who have experience with the issues we advocate on. 
+<br/><br/>
+We worked with students from the University of Washington's Geography Department to create this map in the summer of 2019. 
+
+
+
     </p>
   </div>
 </template>
