5a9ae33a2a7cc23784add7cdfef8ff61dd1a67e8
Adriana Lasso-Harrier <adrianalasso-harrier@Adrianas-MacBook-Air.local>
2019-08-13 21:34:48 -0700

    Changes to file

--- src/components/TheAbout.vue
+++ src/components/TheAbout.vue
@@ -8,7 +8,7 @@ Through in-person community organizing and online advocacy, we advance equitable
 <br/><br/>
 Poverty is a vast issue that is often difficult to define. With this in mind, we chose to create an interactive map that illustrates poverty in Washington state through the lens of the four policy campaigns that our organization works on. Visitors to this map will be guided through a picture of the economic realities for Washington families through depictions of the variable costs of living throughout the state, median income levels, median housing costs, rates of homelessness, and more. Personal story is central to our organization's mission, and we have included the stories of Washingtonians who have experience with the issues we advocate on. 
 <br/><br/>
-We worked with students from the University of Washington's Geography Department to create this map in the summer of 2019. 
+We worked with students from the University of Washington's Geography Deparctment to create this map in the summer of 2019. 
 
 
 
